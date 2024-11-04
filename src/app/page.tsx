9d103bbf025f2bdbcefb26897b6e884e82d862a8
LSigala <lsigala@arkusnexus.com>
2024-11-03 20:43:35 -0600

    Temporada2PeteTheBaker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  async function cakes(e: any){
+    e.preventDefault();
+
+    const data = {
+      RecipeInfo: (e.target.recipeInfo.value)
+    }
+
+    const ingredientsData:any = [];
+    const values:any = [];
+    const recipe = data.RecipeInfo.slice(1).slice(0,-1).split("},")
+    const obtainIngredients = recipe[1].slice(2).slice(0,-1).split(", ")
+    
+    for(const item of obtainIngredients){
+      ingredientsData.push(item.split(":")[0])
+    }
+
+    for(const recipeData of recipe[0].split(", ")){
+      if(!ingredientsData.includes(recipeData.split(":")[0])){
+        values.push(0);
+        break;
+      }
+      
+      for(const ingredientsValues of obtainIngredients){
+        if (recipeData.split(":")[0] == ingredientsValues.split(":")[0]) {
+            const newValue = Number(ingredientsValues.split(":")[1]) / Number(recipeData.split(":")[1])
+            values.push(newValue.toString().split(".")[0]);
+          }
+      }
+    }
+
+    alert(Math.min(values[0]))
+}
+
+  return (
+      <form onSubmit={cakes} className="container col-6 text-center mt-4 bg-light">
+        <div>
+            <div className="d-flex justify-content-center form-group">
+                <div className="form-group col-6 px-md-4 mb-3">
+                    <label>Enter Recipe Information</label>
+                    <input type="text" className="form-control" id="recipeInfo" required aria-describedby="emailHelp " />
+                </div>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3 mx-auto d-block">Send Recipe</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </form>
   );
 }
